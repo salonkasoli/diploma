1f0d4877c85148578993a63564db62fd4a5aac86
Ivan Salosin <salonkasoli@gmail.com>
2019-11-24 19:39:25 +0700

    add search

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,7 @@ from convert import str_to_number
 ## here import stuff to cypher
 from crypt.aes import get_cipher
 from crypt.ope import get_ope_cipher
+from crypt.pailier import get_he_cipher
 from base64 import b64encode, b64decode
 import binascii
 
@@ -18,24 +19,31 @@ def hello_world():
 def input():
     return render_template('input.html')
     
+@app.route('/search')
+def search():
+    return render_template('search.html')
+    
 @app.route('/save', methods=['POST'])
 def save():
     name = request.form.get('name')
     if (name == None or len(name) == 0):
         name = 'empty'
-    #name = str_to_number(name)
     age = check_int(request.form.get('age'))
     therapy_duration = check_int(request.form.get('therapy_duration'))
     gen_before_therapy = check_int(request.form.get('gen_before_therapy'))
     gen_after_therapy = check_int(request.form.get('gen_after_therapy'))
     is_effective = request.form.get('is_effective')
     if (is_effective != None):
-        is_effective = 1
+        is_effective = "true"
     else:
-        is_effective = 0
+        is_effective = "false"
+    #convert it to num
     ciphered_name = det_encrypt_string(name)
     ciphered_age = ope_encrypt_int(age)
-    ciphered_age = ope_encrypt_int(therapy_duration)
+    ciphered_therapy_duration = ope_encrypt_int(therapy_duration)
+    ciphered_gen_before = he_encrypt(gen_before_therapy)
+    ciphered_gen_after = he_encrypt(gen_after_therapy)
+    ciphered_is_effective = det_encrypt_string(is_effective)
     #insert(name, age, therapy_duration, gen_before_therapy, gen_after_therapy, is_effective)
     return redirect(url_for('hello_world'))
     
@@ -65,6 +73,14 @@ def ope_encrypt_int(value):
 def ope_decrypt_int(value):
     print str(value)
     cipher = get_ope_cipher()
+    return cipher.decrypt(value)
+    
+def he_encrypt(value):
+    cipher = get_he_cipher()
+    return cipher.encrypt(value)
+
+def he_decrypt(value):
+    cipher = get_he_cipher()
     return cipher.decrypt(value)
 
 if __name__ == '__main__':
